56bcc706ccbf071f0ca68c9d977a49478dbbe8de
NatalieBlack <natalievictoriablack@gmail.com>
2017-12-08 13:25:29 -0500

    Remove attr_accessors from robot class

--- robot.rb
+++ robot.rb
@@ -1,5 +1,4 @@
 class Robot
-  attr_accessor :needs_repairs, :foreign_model, :vintage_model, :todos, :day_off
 
   def initialize
     @todos = []
@@ -27,6 +26,46 @@ class Robot
 
   def workday?(today)
     today != day_off
+  end
+
+  def needs_repairs
+    @needs_repairs
+  end
+
+  def needs_repairs=(new_val)
+    @needs_repairs = new_val
+  end
+
+  def foreign_model
+    @foreign_model
+  end
+
+  def foreign_model=(new_val)
+    @foreign_model = new_val
+  end
+
+  def vintage_model
+    @vintage_model
+  end
+
+  def vintage_model=(new_val)
+    @vintage_model = new_val
+  end
+
+  def todos 
+    @todos
+  end
+
+  def todos=(new_val)
+    @todos = new_val
+  end
+
+  def day_off 
+    @day_off
+  end
+
+  def day_off=(new_val)
+    @day_off = new_val
   end
 
 end
